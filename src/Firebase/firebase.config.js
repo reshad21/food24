43d2ae231964dded4f3b4da1fe6c891046367cc0
reshad21 <miakala7@gmail.com>
2023-05-08 12:46:29 +0600

    setup firebase confuguration to the app

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAA3Am2xzDSwvw7W1oIUKoQWe5LtPH-crs",
+  authDomain: "food24-94f35.firebaseapp.com",
+  projectId: "food24-94f35",
+  storageBucket: "food24-94f35.appspot.com",
+  messagingSenderId: "848707085500",
+  appId: "1:848707085500:web:27d515fcef7e86eb4a9cb9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
