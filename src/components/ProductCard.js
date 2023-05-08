900e007b7712b2e41f136e8f184506a5ae77c1d1
reshad21 <miakala7@gmail.com>
2023-05-08 16:37:32 +0600

    sound added successfuly

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import productImage from '../assets/0be61272117251004060346e57b7b6e6.jpg';
+import buttonClickSound from '../assets/mixkit-on-or-off-light-switch-tap-2585.wav';
 
 const ProductCard = ({ product }) => {
     // const { pathname } = useLocation();
+    // Function to play sound effect when button is clicked
+    const playButtonClickSound = () => {
+        const audio = new Audio(buttonClickSound);
+        audio.play();
+    }
+
+    // UseEffect hook to load the sound effect on component mount
+    useEffect(() => {
+        new Audio(buttonClickSound);
+    }, []);
     return (
         <div className='shadow-lg relative rounded-3xl border p-3 flex flex-col text-indigo-900'>
             <div className='rounded-full grid place-items-center absolute top-2 right-2 bg-indigo-500 text-white h-8 w-8 font-bold '>
@@ -15,9 +26,7 @@ const ProductCard = ({ product }) => {
             <p className='text-center font-semibold mb-3'>Rating: 5</p>
 
             <div className='flex gap-2 mt-5'>
-                <button
-                    className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
-                >
+                <button onClick={playButtonClickSound} className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
                     Add to item
                 </button>
             </div>
